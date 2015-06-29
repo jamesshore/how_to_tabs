671950f02b1e1df226669d180294056e1c442f22
James Shore <jshore@jamesshore.com>
2015-06-29 11:25:03 -0700

    Build script checks Node version

--- Jakefile.js
+++ Jakefile.js
@@ -2,9 +2,21 @@
 (function() {
 	"use strict";
 
+	var EXPECTED_NODE_VERSION = "v0.12.4";
+
 	desc("Default build");
-	task("default", function() {
+	task("default", [ "version" ], function() {
 		console.log("\n\nBUILD OK");
+	});
+
+	desc("Check Node version");
+	task("version", function() {
+		console.log("Checking Node version: .");
+
+		var actualVersion = process.version;
+		if (actualVersion !== EXPECTED_NODE_VERSION) {
+			fail("Incorrect Node version: expected " + EXPECTED_NODE_VERSION + ", but was " + actualVersion);
+		}
 	});
 
 }());
