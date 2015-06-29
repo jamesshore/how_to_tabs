3f42d98c16a4f9dc29e4fcb7194521ba0314e6e9
James Shore <jshore@jamesshore.com>
2015-06-29 10:53:53 -0700

    Rebuild npm modules when necessary

--- jake.cmd
+++ jake.cmd
@@ -1,3 +1,9 @@
 @echo off
 
+if exist node_modules\.bin\jake goto :run_jake
+
+echo Building npm modules:
+npm rebuild
+
+:run_jake
 node_modules\.bin\jake %*
--- jake.sh
+++ jake.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+[ ! -f node_modules/.bin/jake ] && echo "Building npm modules:" && npm rebuild
+
 node_modules/.bin/jake $*
