5d98de6f861bebf97e19e1afd2d9765b3b0c3347
James Shore <jshore@jamesshore.com>
2015-08-06 12:51:40 -0700

    Example tests use Mocha

--- src/test.js
+++ src/test.js
@@ -4,11 +4,17 @@
 
 	var assert = require("chai").assert;
 
-	// basic addition
-	assert.equal(add(3, 4), 7);
+	describe("Addition", function() {
 
-	// IEEE 754 floating point
-	assert.equal(add(0.1, 0.2), 0.30000000000000004);
+		it("adds positive numbers", function() {
+			assert.equal(add(3, 4), 7);
+		});
+
+		it("uses IEEE 754 floating point", function() {
+			assert.equal(add(0.1, 0.2), 0.30000000000000004);
+		});
+
+	});
 
 
 	function add(a, b) {
