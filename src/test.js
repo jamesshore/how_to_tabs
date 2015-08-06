3ec6d61b651623682b3a59f88c3c1c235ca9842d
James Shore <jshore@jamesshore.com>
2015-08-06 12:38:52 -0700

    Example test uses Chai for assertions

--- src/test.js
+++ src/test.js
@@ -2,14 +2,12 @@
 (function() {
 	"use strict";
 
-	assertEqual(add(3, 4), 7);
+	var assert = require("chai").assert;
+
+	assert.equal(add(3, 4), 7);
 
 	function add(a, b) {
 		return a + b;
-	}
-
-	function assertEqual(actual, expected) {
-		if (actual !== expected) throw new Error("Expected " + expected + ", but got " + actual);
 	}
 
 }());
