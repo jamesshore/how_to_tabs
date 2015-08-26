a0c9001f16fd23c30d28a206b4f169b6b8f2e781
James Shore <jshore@jamesshore.com>
2015-08-26 15:40:20 -0700

    Configured Karma and got tests working

--- src/test.js
+++ src/test.js
@@ -2,17 +2,19 @@
 (function() {
 	"use strict";
 
-	var assert = require("chai").assert;
-
 	describe("Addition", function() {
 
 		it("adds positive numbers", function() {
-			assert.equal(add(3, 4), 7);
+			assertEqual(add(3, 4), 7);
 		});
 
 		it("uses IEEE 754 floating point", function() {
-			assert.equal(add(0.1, 0.2), 0.30000000000000004);
+			assertEqual(add(0.1, 0.2), 0.30000000000000004);
 		});
+
+		function assertEqual(actual, expected) {
+			if (actual !== expected) throw new Error("expected " + expected + ", but was " + actual);
+		}
 
 	});
 
