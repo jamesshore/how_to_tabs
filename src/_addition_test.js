5c54dc93a395f2ff1e1242421384ef43170edf72
James Shore <jshore@jamesshore.com>
2015-09-08 18:03:14 -0700

    Factored out example test into multiple modules

--- src/_addition_test.js
+++ src/_addition_test.js
@@ -2,14 +2,16 @@
 (function() {
 	"use strict";
 
+	var addition = require("./addition.js");
+
 	describe("Addition", function() {
 
 		it("adds positive numbers", function() {
-			assertEqual(add(3, 4), 7);
+			assertEqual(addition.add(3, 4), 7);
 		});
 
 		it("uses IEEE 754 floating point", function() {
-			assertEqual(add(0.1, 0.2), 0.30000000000000004);
+			assertEqual(addition.add(0.1, 0.2), 0.30000000000000004);
 		});
 
 		function assertEqual(actual, expected) {
@@ -17,10 +19,5 @@
 		}
 
 	});
-
-
-	function add(a, b) {
-		return a + b;
-	}
 
 }());
---
+++ src/addition.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2015 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
+(function() {
+	"use strict";
+
+	exports.add = function add(a, b) {
+		return a + b;
+	};
+
+}());
