b535e5b09399954c0cd153d01c201296ffb39d30
James Shore <jshore@jamesshore.com>
2015-09-08 18:51:35 -0700

    Created simple demonstration app

---
+++ src/app.js
@@ -0,0 +1,10 @@
+// Copyright (c) 2015 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
+(function() {
+	"use strict";
+
+	var addition = require("./addition.js");
+
+	console.log("Hello!");
+	console.log("42 + 13 = " + addition.add(42, 13));
+
+}());
