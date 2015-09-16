f27a32c4e9f7c0421d522b9ba9f815a3011615a5
James Shore <jshore@jamesshore.com>
2015-09-16 13:49:31 -0700

    Isolated chai behind assert.js wrapper

--- src/javascript/_addition_test.js
+++ src/javascript/_addition_test.js
@@ -3,7 +3,7 @@
 	"use strict";
 
 	var addition = require("./addition.js");
-	var assert = require("../vendor/chai-2.1.0.js").assert;
+	var assert = require("./assert.js");
 
 	describe("Addition", function() {
 
---
+++ src/javascript/assert.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2015 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
+(function() {
+	"use strict";
+
+	var assert = require("../vendor/chai-2.1.0.js").assert;
+
+	module.exports = assert;
+
+}());
