e722d2885906a6e0397ed4fc4e771246931b3462
James Shore <jshore@jamesshore.com>
2015-09-16 12:26:59 -0700

    Finished 'clean' task in build script

--- Jakefile.js
+++ Jakefile.js
@@ -7,7 +7,7 @@
 	var semver = require("semver");
 	var jshint = require("simplebuild-jshint");
 	var karma = require("simplebuild-karma");
-	var shell = require("shell");
+	var shell = require("shelljs");
 
 	var KARMA_CONFIG = "karma.conf.js";
 	var DIST_DIR = "generated/dist";
