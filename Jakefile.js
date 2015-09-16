4b9d813700427ccec6d23468786231243689b909
James Shore <jshore@jamesshore.com>
2015-09-16 12:40:35 -0700

    'build' task bundles JavaScript with Browerify

--- Jakefile.js
+++ Jakefile.js
@@ -89,6 +89,12 @@
 
 		shell.rm("-rf", DIST_DIR + "/*");
 		shell.cp("src/index.html", DIST_DIR);
+
+		jake.exec(
+			"node node_modules/browserify/bin/cmd.js src/app.js -o " + DIST_DIR + "/bundle.js",
+			{ interactive: true },
+			complete
+		);
 	});
 
 	directory(DIST_DIR);
