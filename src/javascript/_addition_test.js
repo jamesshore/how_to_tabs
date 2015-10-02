9ea9c440e2e5013fa813986a832b11e9ea20931e
James Shore <jshore@jamesshore.com>
2015-10-02 15:55:28 -0700

    Demonstrated DOM manipulation

--- src/javascript/_addition_test.js
+++ src/javascript/_addition_test.js
@@ -7,6 +7,22 @@
 	describe("Something", function() {
 
 		it("something", function() {
+
+			var div = document.createElement("div");
+
+			div.innerHTML = "This is an example.";
+
+			document.body.appendChild(div);
+
+
+			var p = document.createElement("p");
+			p.innerHTML = "This is an embedded paragraph";
+			div.appendChild(p);
+
+
+
+			div.parentNode.removeChild(div);
+
 		});
 
 	});
