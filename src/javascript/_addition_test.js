cff9e738e49eb21e93e5337de8171e737b79a100
James Shore <jshore@jamesshore.com>
2015-10-02 15:42:45 -0700

    Set up bare-bones tab example

--- src/javascript/_addition_test.js
+++ src/javascript/_addition_test.js
@@ -2,26 +2,11 @@
 (function() {
 	"use strict";
 
-	var addition = require("./addition.js");
 	var assert = require("./assert.js");
 
-	describe("Addition", function() {
+	describe("Something", function() {
 
-		it("adds positive numbers", function() {
-			assert.equal(addition.add(3, 4), 7);
-		});
-
-		it("uses IEEE 754 floating point", function() {
-			assert.equal(addition.add(0.1, 0.2), 0.30000000000000004);
-		});
-
-	});
-
-
-	describe("Subtraction", function() {
-
-		it("subtracts positive numbers", function() {
-			assert.equal(addition.subtract(10, 3), 7);
+		it("something", function() {
 		});
 
 	});
--- src/javascript/app.js
+++ src/javascript/app.js
@@ -2,9 +2,4 @@
 (function() {
 	"use strict";
 
-	var addition = require("./addition.js");
-
-	console.log("Hello!");
-	console.log("42 + 13 = " + addition.add(42, 13));
-
 }());
