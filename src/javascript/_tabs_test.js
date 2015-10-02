08bef1207431a2a27764eef5038eff76fc90d00e
James Shore <jshore@jamesshore.com>
2015-10-02 16:23:27 -0700

    Created tab library (doesn't do anything yet)

---
+++ src/javascript/_tabs_test.js
@@ -0,0 +1,24 @@
+// Copyright (c) 2015 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
+(function() {
+	"use strict";
+
+	var assert = require("./assert.js");
+	var tabs = require("./tabs.js");
+
+	describe("Tabs", function() {
+
+		it("has an API", function() {
+
+			tabs.initialize();
+
+
+
+			//var div = document.createElement("div");
+			//div.innerHTML = "This is an example.";
+			//document.body.appendChild(div);
+			//div.parentNode.removeChild(div);
+		});
+
+	});
+
+}());
---
+++ src/javascript/tabs.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2015 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
+(function() {
+	"use strict";
+
+	exports.initialize = function initialize() {
+
+	};
+
+}());
