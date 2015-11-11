44a2bafedea234a8606e4c8c42033b181531992c
James Shore <jshore@jamesshore.com>
2015-11-10 16:30:41 -0800

    tabs.initialize() no longer overwrites existing classes

--- src/javascript/_tabs_test.js
+++ src/javascript/_tabs_test.js
@@ -7,12 +7,23 @@
 
 	describe("Tabs", function() {
 
-		it("sets a class on an element", function() {
+		it("sets a class on an element when that element has no existing classes", function() {
 			var element = addElement("div");
 
 			tabs.initialize(element, "someClass");
 
 			assert.equal(getClass(element), "someClass");
+
+			removeElement(element);
+		});
+
+		it("sets a class on an element without erasing existing classes", function() {
+			var element = addElement("div");
+			element.setAttribute("class", "existingClass");
+
+			tabs.initialize(element, "newClass");
+
+			assert.equal(getClass(element), "existingClass newClass");
 
 			removeElement(element);
 		});
--- src/javascript/tabs.js
+++ src/javascript/tabs.js
@@ -3,7 +3,12 @@
 	"use strict";
 
 	exports.initialize = function initialize(element, className) {
-		element.setAttribute("class", className);
+		var classes = element.getAttribute("class");
+
+		if (classes === null) classes = className;
+		else classes += " " + className;
+
+		element.setAttribute("class", classes);
 	};
 
 }());
