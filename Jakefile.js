a24646eed84bc81ec317bf661e87bb7f62aadbe9
James Shore <jshore@jamesshore.com>
2015-11-28 12:03:25 -0800

    Upgraded Mobile Safari 8 (iOS 8.4) --> 9 (iOS 9.1.0)

--- Jakefile.js
+++ Jakefile.js
@@ -76,7 +76,7 @@
 				"IE 11.0.0 (Windows 7 0.0.0)",
 				"IE 9.0.0 (Windows 7 0.0.0)",
 				"Safari 9.0.1 (Mac OS X 10.10.5)",
-				"Mobile Safari 8.0.0 (iOS 8.4.0)",
+				"Mobile Safari 9.0.0 (iOS 9.1.0)",
 				"Chrome Mobile 44.0.2403 (Android 6.0.0)"
 			],
 			strict: !process.env.loose
