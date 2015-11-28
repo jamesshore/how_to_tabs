ad211bf9faff83b98f36e28df4d64c0b8c85b7f0
James Shore <jshore@jamesshore.com>
2015-11-28 13:03:54 -0800

    Factored out option checking code

--- src/javascript/tabs.js
+++ src/javascript/tabs.js
@@ -13,11 +13,11 @@
 		var activeTabClass = options.activeTabClass;
 		var contentHideClass = options.contentHideClass;
 
-		if (tabs === undefined) throw new Error("Expected options.tabs");
-		if (content === undefined) throw new Error("Expected options.content");
-		if (defaultElement === undefined) throw new Error("Expected options.default");
-		if (activeTabClass === undefined) throw new Error("Expected options.activeTabClass");
-		if (contentHideClass === undefined) throw new Error("Expected options.contentHideClass");
+		checkOption(tabs, "options.tabs");
+		checkOption(content, "options.content");
+		checkOption(defaultElement, "options.defaultElement");
+		checkOption(activeTabClass, "options.activeTabClass");
+		checkOption(contentHideClass, "options.contentHideClass");
 
 		content.forEach(function(element) {
 			element.classList.add(contentHideClass);
@@ -33,6 +33,10 @@
 			if (contentTabs[i] === defaultContentTab) return i;
 		}
 		throw new Error("Could not find default in list");
+	}
+
+	function checkOption(option, name) {
+		if (option === undefined) throw new Error("Expected " + name);
 	}
 
 }());
