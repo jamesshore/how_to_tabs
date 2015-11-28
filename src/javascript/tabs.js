424bdb52895b309eab866ab433401b90549f843c
James Shore <jshore@jamesshore.com>
2015-11-28 13:35:09 -0800

    Factored out showTab() method

--- src/javascript/tabs.js
+++ src/javascript/tabs.js
@@ -19,17 +19,19 @@
 		checkOption(activeTabClass, "options.activeTabClass");
 		checkOption(hiddenContentClass, "options.hiddenContentClass");
 
+		showTab(defaultTab, tabs, content, activeTabClass, hiddenContentClass);
+	};
 
-		var activeIndex = findIndexOfDefaultElement(tabs, defaultTab);
+	function showTab(tabToShow, tabs, content, activeTabClass, hiddenContentClass) {
+		var activeIndex = findIndexOfDefaultElement(tabs, tabToShow);
 		var defaultContent = content[activeIndex];
 
 		content.forEach(function(element) {
 			element.classList.add(hiddenContentClass);
 		});
-
 		defaultContent.classList.remove(hiddenContentClass);
-		defaultTab.classList.add(activeTabClass);
-	};
+		tabToShow.classList.add(activeTabClass);
+	}
 
 	function findIndexOfDefaultElement(contentTabs, defaultContentTab) {
 		for (var i = 0; i < contentTabs.length; i++) {
