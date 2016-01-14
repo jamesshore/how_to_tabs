876a7761b304160d0dc31261533b5995ddfff36d
James Shore <jshore@jamesshore.com>
2016-01-13 19:35:00 -0800

    Wired up tab library to index.html

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -2,4 +2,25 @@
 (function() {
 	"use strict";
 
+	var tabs = require("./tabs.js");
+
+	document.addEventListener("DOMContentLoaded", function() {
+		var tab1 = document.getElementById("tab1");
+		var tab2 = document.getElementById("tab2");
+		var tab3 = document.getElementById("tab3");
+
+		var content1 = document.getElementById("content1");
+		var content2 = document.getElementById("content2");
+		var content3 = document.getElementById("content3");
+
+		tabs.initialize({
+			tabs: [ tab1, tab2, tab3 ],
+			content: [ content1, content2, content3 ],
+			defaultTab: tab1,
+			activeTabClass: "active",
+			hiddenContentClass: "hidden"
+		});
+	});
+
+
 }());
