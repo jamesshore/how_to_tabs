4c01b182b5a145133b234bc8a555d7d4937e3890
James Shore <jshore@jamesshore.com>
2016-01-13 18:16:21 -0800

    Upgraded iOS 9.1.0 --> 9.2.0

--- Jakefile.js
+++ Jakefile.js
@@ -76,7 +76,7 @@
 				"IE 11.0.0 (Windows 7 0.0.0)",
 				"IE 9.0.0 (Windows 7 0.0.0)",
 				"Safari 9.0.2 (Mac OS X 10.10.5)",
-				"Mobile Safari 9.0.0 (iOS 9.1.0)",
+				"Mobile Safari 9.0.0 (iOS 9.2.0)",
 				"Chrome Mobile 44.0.2403 (Android 6.0.0)"
 			],
 			strict: !process.env.loose
