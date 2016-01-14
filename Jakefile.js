6440a5180592ff4736951dab0abd28fc2be18fc5
James Shore <jshore@jamesshore.com>
2016-01-13 18:15:42 -0800

    Upgraded Chrome 46 --> 47

--- Jakefile.js
+++ Jakefile.js
@@ -72,7 +72,7 @@
 			configFile: KARMA_CONFIG,
 			expectedBrowsers: [
 				"Firefox 43.0.0 (Mac OS X 10.10.0)",
-				"Chrome 46.0.2490 (Mac OS X 10.10.5)",
+				"Chrome 47.0.2526 (Mac OS X 10.10.5)",
 				"IE 11.0.0 (Windows 7 0.0.0)",
 				"IE 9.0.0 (Windows 7 0.0.0)",
 				"Safari 9.0.1 (Mac OS X 10.10.5)",
